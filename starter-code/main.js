250ad8e6202cce6968ab1292956a60bd0d74a8ef
Rubi SC <rubisc@gmail.com>
2017-01-17 17:22:45 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
 console.log("JS file is connected to HTML! Woo!")
+/*below is the name of each card and the face they have*/
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
