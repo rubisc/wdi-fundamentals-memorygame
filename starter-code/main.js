daf3fd94bbeda6102efb4a4a8f9e676116c7e905
Rubi SC <rubisc@gmail.com>
2017-01-18 12:11:01 -0800

    created 9.5 java exercise

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+  alert ('Sorry, this is not a match. Give it anothr try.');
+} else if (cardOne === cardTwo) {
+  alert ('Congrats! You found a match.');
+} else if (cardThree === cardFour) {
+  alert ('Congrats! You found a match.');
+} else
+  alert ('Sorry, this is not a match. Give it anothr try.');
+  }
