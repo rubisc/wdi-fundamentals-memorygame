78f90ac073c6e91dd7e6c6b6e74bab46f22d2176
Rubi SC <rubisc@gmail.com>
2017-01-18 21:25:38 -0800

    unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardTwo === cardFour) {
   alert ('Sorry, this is not a match. Give it another try.');
 } else if (cardOne === cardTwo) {
@@ -14,3 +15,14 @@ if (cardTwo === cardFour) {
 } else
   alert ('Sorry, this is not a match. Give it another try.');
   }
+  */
+var createCards = function() {
+var gameBoard = document.getElementById('game-board');
+ for(var i = 1; i <= 4; i++) {
+   var newCard = document.createElement("div");
+   newCard.className = "card";
+    gameBoard.appendChild(newCard);
+ }
+}
+
+createCards();
